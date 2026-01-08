f7b22933d61a2bab5a7489ff7ad4e0f5c8b61066
truesweeter <thebestmaxbelov@gmail.com>
2026-01-08 20:00:24 +0300

    add tiles

--- col.tsx
+++ col.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.2" name="col" tilewidth="70" tileheight="70" spacing="2" tilecount="1" columns="1">
- <image source="../../../../Program Files/Tiled/Tiles/boxItem.png" width="70" height="70"/>
+ <image source="assets/tiles/boxItem.png" width="70" height="70"/>
 </tileset>
