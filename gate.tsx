92f58e60c4fd99550b30055d04631a28964aa4da
truesweeter <thebestmaxbelov@gmail.com>
2026-01-25 21:26:57 +0300

    bug fix

--- gate.tsx
+++ gate.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.2" name="gate" tilewidth="70" tileheight="70" spacing="2" tilecount="1" columns="1">
- <image source="../../../../Program Files/Tiled/Tiles/boxCoin.png" width="70" height="70"/>
+ <image source="assets/tiles/boxCoin.png" width="70" height="70"/>
 </tileset>
